ee8e33404f2bc81ba3c064ceff0b0763c8600ff5
bastb <bastb@tenberge-ict.nl>
2014-08-23 00:37:16 +0200

    Added setup.py to simplify installation and moved the existing code into a package folder. Selected version 0.6 without any particular reason.
    Might want to check with the original author; I found a reference to GPLv2 (or later, at your choosing) in the comments, so I assumed it was released under GPL. Same goes for the email address.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#
+# setup-ified it
+#
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pyx509',
+    version = '0.6', # Would really like to link this to a tag/branch/whatever
+    install_requires = ['pyasn1 >= 0.1.4',],
+    author = 'hiviah',
+    author_email = 'hiviah@users.github.com',
+    license = 'GPL',
+    description = 'X.509 Certificate Parser for Python',
+    url = 'https://github.com/hiviah/pyx509',
+    classifiers = 'alpha',
+    packages = find_packages(),
+)
