fd6df56d9abc2c24e951261984c4eb573d4d65a0
Ondrej Mikle <ondrej.mikle@nic.cz>
2014-08-28 12:23:33 +0200

    Fixed required packages for installation

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ from setuptools import setup, find_packages
 setup(
     name = 'pyx509',
     version = '0.6.0', # Would really like to link this to a tag/branch/whatever
-    install_requires = ['pyasn1 (>= 0.1.4)',],
-    author = 'hiviah',
-    author_email = 'hiviah@users.github.com',
+    equires = ['pyasn1 (>= 0.1.4)',],
+    author = 'Ondrej Mikle',
+    author_email = 'ondrej.mikle@gmail.com',
     license = 'GPL',
     description = 'X.509 Certificate Parser for Python',
     url = 'https://github.com/hiviah/pyx509',
